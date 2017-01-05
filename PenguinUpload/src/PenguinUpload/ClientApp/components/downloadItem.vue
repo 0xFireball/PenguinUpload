e2d54009cc07753cd3b2d71a6e3f0f276fa68de1
0xFireball <0xFireball@outlook.com>
2017-01-04 16:17:38 -0800

    add support for downloading password files

--- PenguinUpload/src/PenguinUpload/ClientApp/components/downloadItem.vue
+++ PenguinUpload/src/PenguinUpload/ClientApp/components/downloadItem.vue
@@ -18,7 +18,12 @@
           <p>{{ file.sizeText }}</p>
         </md-card-content>
         <md-card-actions v-if="!error">
-          <md-button @click="downloadFile">Download</md-button>
+          <md-button @click="downloadFile">
+            <md-icon v-if="file.pass.length > 0">lock</md-icon>
+            <span>
+              Download
+            </span>
+          </md-button>
           <!--<md-button>Copy Link</md-button>-->
         </md-card-actions>
       </div>
@@ -28,6 +33,11 @@
 
 <script>
   import axios from 'axios'
+  let axiosRequestConfig = {
+    validateStatus: function (status) {
+      return status >= 200 && status < 500
+    }
+  }
 
   export default {
     props: ['itemId'],
@@ -38,22 +48,38 @@
           name: 'Loading',
           size: 'retrieving information from server',
           id: null,
+          pass: ''
         },
         error: false
       }
     },
     methods: {
       downloadFile: function () {
-        window.location.href = '/api/download/' + this.file.id
+        window.location.href = '/api/download/' + this.file.id + this.file.pass ? '!' + this.file.pass : ''
       },
-      updateFileInfo: function () {
+      updateFileInfo: function (key) {
         let vm = this
+        let suffix = key ? '!' + key : ''
         vm.file.id = vm.itemId
-        axios.get('/api/fileInfo/' + vm.file.id)
+        axios.get('/api/fileInfo/' + vm.file.id + suffix, axiosRequestConfig)
           .then(function (response) {
-            vm.file.name = response.data.name
-            vm.file.sizeText = response.data.hrSize
-            vm.loading = false
+            if (response.status == 200) {
+              vm.file.name = response.data.name
+              vm.file.sizeText = response.data.hrSize
+              vm.loading = false
+              if (suffix) {
+                vm.file.pass = key
+              }
+            } else if (response.status == 401) {
+              // file is password protected
+              vm.$root.showPrompt('Enter password', 'File password', function (r) {
+                if (r) {
+                  setTimeout(() => {
+                    vm.updateFileInfo(r)
+                  }, 500)
+                }
+              })
+            }
           })
           .catch(function (error) {
             console.log(error)
