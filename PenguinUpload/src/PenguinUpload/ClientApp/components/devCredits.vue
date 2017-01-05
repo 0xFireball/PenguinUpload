626778e77486599d557522c1e8fa3c793c4a8198
0xFireball <0xFireball@outlook.com>
2017-01-04 16:59:43 -0800

    add link to iridiumion

--- PenguinUpload/src/PenguinUpload/ClientApp/components/devCredits.vue
+++ PenguinUpload/src/PenguinUpload/ClientApp/components/devCredits.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="dev-credits">
-    <h6 class="credit-text">Made by <a href="https://0xfireball.me" target="_blank">0xFireball</a></h6>
+    <h6 class="credit-text">Made by <a href="https://0xfireball.me" target="_blank">0xFireball</a> of <a href="https://iridiumion.xyz" target="_blank">IridiumIon Software</a></h6>
   </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
+  export default {
+    data: function () {
+      return {
+      }
     }
   }
-}
 </script>
 
 <style scoped>
