a7d2549384ba6e0d392dda2c4d8aa95eeb6abdf1
0xFireball <0xFireball@outlook.com>
2017-01-13 13:17:13 -0800

    remove card on files

--- PenguinUpload/src/PenguinUpload/ClientApp/components/myFilesList.vue
+++ PenguinUpload/src/PenguinUpload/ClientApp/components/myFilesList.vue
@@ -7,51 +7,45 @@
             <md-button class="md-raised md-primary" @click="uploadMoreFiles">Upload Files</md-button>
           </div>
           <div class="uploaded-files-list">
-            <md-card>
-              <md-card-header>
-                <div class="md-title">All My Files</div>
-                <div class="md-subtitle">Files</div>
-              </md-card-header>
-              <md-card-content>
-                <div v-if="loadFinished">
-                  <div v-if="noFiles">
-                    <p>No Files</p>
+            <div class="md-title">All My Files</div>
+            <div class="md-subtitle">Files</div>
+            <div v-if="loadFinished">
+              <div v-if="noFiles">
+                <p>No Files</p>
+              </div>
+              <md-list class="custom-list md-double-line">
+                <md-list-item v-for="(file, ix) in files">
+                  <md-icon class="md-primary">cloud_done</md-icon>
+                  <div class="md-list-text-container">
+                    <span> {{ file.name }}</span>
+                    <span> {{ file.hrSize }}</span>
                   </div>
-                  <md-list class="custom-list md-double-line">
-                    <md-list-item v-for="(file, ix) in files">
-                      <md-icon class="md-primary">cloud_done</md-icon>
-                      <div class="md-list-text-container">
-                        <span> {{ file.name }}</span>
-                        <span> {{ file.hrSize }}</span>
-                      </div>
-                      <md-button class="md-icon-button md-list-action" @click="lockFile(ix)" v-if="!file.locked">
-                        <md-icon class="md-primary">lock_open</md-icon>
-                      </md-button>
-                      <md-button class="md-icon-button md-list-action" @click="unlockFile(ix)" v-else>
-                        <md-icon class="md-primary">lock</md-icon>
-                      </md-button>
-                      <md-button class="md-icon-button md-list-action" @click="downloadFile(ix)">
-                        <md-icon class="md-primary">file_download</md-icon>
-                      </md-button>
-                      <md-menu md-align-trigger>
-                        <md-button class="md-icon-button md-list-action" md-menu-trigger>
-                          <md-icon class="md-primary">more_horiz</md-icon>
-                        </md-button>
-                        <md-menu-content>
-                          <md-menu-item @click="visitDownloadPage(ix)">Download Page</md-menu-item>
-                          <md-menu-item @click="deleteFile(ix)">Delete</md-menu-item>
-                        </md-menu-content>
-                      </md-menu>
-                      <md-divider class="md-inset"></md-divider>
-                    </md-list-item>
-                  </md-list>
-                </div>
-                <div v-else>
-                  <md-spinner md-indeterminate></md-spinner>
-                  <h5>Retrieving Data</h5>
-                </div>
-              </md-card-content>
-            </md-card>
+                  <md-button class="md-icon-button md-list-action" @click="lockFile(ix)" v-if="!file.locked">
+                    <md-icon class="md-primary">lock_open</md-icon>
+                  </md-button>
+                  <md-button class="md-icon-button md-list-action" @click="unlockFile(ix)" v-else>
+                    <md-icon class="md-primary">lock</md-icon>
+                  </md-button>
+                  <md-button class="md-icon-button md-list-action" @click="downloadFile(ix)">
+                    <md-icon class="md-primary">file_download</md-icon>
+                  </md-button>
+                  <md-menu md-align-trigger>
+                    <md-button class="md-icon-button md-list-action" md-menu-trigger>
+                      <md-icon class="md-primary">more_horiz</md-icon>
+                    </md-button>
+                    <md-menu-content>
+                      <md-menu-item @click="visitDownloadPage(ix)">Download Page</md-menu-item>
+                      <md-menu-item @click="deleteFile(ix)">Delete</md-menu-item>
+                    </md-menu-content>
+                  </md-menu>
+                  <md-divider class="md-inset"></md-divider>
+                </md-list-item>
+              </md-list>
+            </div>
+            <div v-else>
+              <md-spinner md-indeterminate></md-spinner>
+              <h5>Retrieving Data</h5>
+            </div>
           </div>
         </div>
       </div>
@@ -98,12 +92,14 @@
         let f = this.files[ix]
         if (f.locked) {
           let vm = this
-          vm.$root.showConfirm(`
+          vm.$root.showConfirm(
+            `
 This file is password protected. Do you want to encode the <br>
 password in the link? If you don't do this, anyone who visits <br>
 your link will need to enter the file password to view the file.
 `,
-            'Include password?', function (r) {
+            'Include password?',
+            function (r) {
               if (r) {
                 // include password
                 setTimeout(function () {
@@ -128,10 +124,12 @@ your link will need to enter the file password to view the file.
           .then(function (res) {
             // password should be returned
             let dlPage = window.location.href.split("#")[0] + '#/d/' + f.fileId + '/' + window.btoa(res.data)
-            vm.$root.showPopup(`
+            vm.$root.showPopup(
+              `
 Download link with password encoded:<br>
 <a target="_blank" href="${dlPage}">${dlPage}</a>
-            `, 'Link Created')
+            `,
+              'Link Created')
           })
       },
       downloadFile: function (ix) {
@@ -170,16 +168,17 @@ Download link with password encoded:<br>
       deleteFile: function (ix) {
         let vm = this
         let f = vm.files[ix]
-        vm.$root.showConfirm('Are you sure you want to delete this file? It cannot be recovered.', 'Confirm Delete', (r) => {
-          if (r) {
-            // send delete request
-            axios.delete('/api/delete/' + f.fileId, vm.authRequestParams)
-              .then(function (res) {
-                // update file list
-                vm.files.splice(ix, 1)
-              })
-          }
-        })
+        vm.$root.showConfirm('Are you sure you want to delete this file? It cannot be recovered.', 'Confirm Delete',
+          (r) => {
+            if (r) {
+              // send delete request
+              axios.delete('/api/delete/' + f.fileId, vm.authRequestParams)
+                .then(function (res) {
+                  // update file list
+                  vm.files.splice(ix, 1)
+                })
+            }
+          })
       }
     },
     mounted: function () {
