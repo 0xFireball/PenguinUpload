81de221d9ef35be6309747661741ea2d54b3c0bf
Nihal Talur <0xFireball@outlook.com>
2017-01-24 10:48:37 -0800

    add build script

---
+++ build/build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+#set -e
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then  
+  rm -R $artifactsFolder
+fi
+
+# Dotnet info
+
+dotnet --version
+
+echo "Restoring packages..."
+# Restore packages
+dotnet restore
+
+echo "Building project..."
+
+# Build
+dotnet build -c Release PenguinUpload/src/PenguinUpload
+
+echo "Running tests...
+
+echo "Publishing project..."
+
+# Publish
+dotnet publish -c Release PenguinUpload/src/PenguinUpload
