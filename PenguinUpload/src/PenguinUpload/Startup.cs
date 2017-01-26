218317d3d174131a6d76dfc4261630b41ea5128f
0xFireball <0xFireball@outlook.com>
2017-01-25 18:56:44 -0800

    cleanup

--- PenguinUpload/src/PenguinUpload/Startup.cs
+++ PenguinUpload/src/PenguinUpload/Startup.cs
@@ -61,10 +61,6 @@ namespace PenguinUpload
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-//
-//                routes.MapSpaFallbackRoute(
-//                    name: "spa-fallback",
-//                    defaults: new { controller = "Home", action = "Index" });
             });
         }
     }
