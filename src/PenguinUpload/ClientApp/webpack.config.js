cbab09f4035efe82dccf822c584ce7a76954a73c
0xFireball <0xFireball@outlook.com>
2017-05-28 16:09:52 -0700

    update output and entry config

--- src/PenguinUpload/ClientApp/webpack.config.js
+++ src/PenguinUpload/ClientApp/webpack.config.js
@@ -2,9 +2,11 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    main: './src/main.js'
+  },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../wwwroot/dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
