f0ed00e448d0953ad3a0286932fa7b88ec7051b5
0xFireball <0xFireball@outlook.com>
2017-05-28 17:58:09 -0700

    move components

--- src/PenguinUpload/ClientApp/src/pages/Register.vue
+++ src/PenguinUpload/ClientApp/src/pages/Register.vue
@@ -1,27 +1,27 @@
 <template>
   <div>
     <div class="center">
-      <img src="public/icon.png" />
-      <h2>{{ appName }}</h2>
-    </div>
-    <div>
-      <p>
-        {{ appName }} is a game. Welcome to about.
-      </p>
+      <h2>Register</h2>
+      <v-layout row>
+        <v-flex xs12 lg6 offset-lg3>
+          <login-form mode="register"></login-form>
+        </v-flex xs8>
+      </v-layout>
     </div>
   </div>
 </template>
 
 <script>
+
+import loginForm from '../components/loginForm.vue'
+
 export default {
   data () {
     return {
     }
   },
-  computed: {
-    appName: function () {
-      return this.$store.state.data.appName
-    }
+  components: {
+    loginForm
   }
 }
 </script>
