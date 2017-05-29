578e2b79609260f822df8b8ac366221109f94fab
0xFireball <0xFireball@outlook.com>
2017-05-28 18:14:21 -0700

    add some client script

--- src/PenguinUpload/ClientApp/src/api/penguinupload.js
+++ src/PenguinUpload/ClientApp/src/api/penguinupload.js
@@ -6,5 +6,27 @@
 import axios from 'axios'
 
 export class PenguinUploadApi {
-  
+  constructor (endpoint, key = null) {
+    this.ep = endpoint
+    this.init(key)
+  }
+
+  static create (endpoint, apikey = null) {
+    return new Promise((resolve, reject) => {
+      let api = new PenguinUploadApi(endpoint, apikey)
+      resolve(api)
+    })
+  }
+
+  init (key = null) {
+    this.key = key
+    this.username = null
+    this.axios = axios.create({
+      baseURL: this.ep,
+      headers: {
+        Authorization: this.key
+      },
+      responseType: 'json'
+    })
+  }
 }
